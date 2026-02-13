e71eace9dc1de1cf80c69fa49c7ddaaf1ad52ec4
aakiphasanmutakin-stack <aakiphasanmutakin@gmail.com>
2026-02-14 02:35:26 +0700

    Add files via upload

---
+++ tugas.php
@@ -0,0 +1,28 @@
+<?php
+class KalkulatorSuhu {
+    private $celsius;
+
+    public function __construct($celsius) {
+        $this->celsius = $celsius;
+    }
+
+    public function keFahrenheit() {
+        return ($this->celsius * 9/5) + 32;
+    }
+
+    public function keKelvin() {
+        return $this->celsius + 273.15;
+    }
+
+    public function getCelsius() {
+        return $this->celsius;
+    }
+}
+
+// Contoh penggunaan
+$suhu = new KalkulatorSuhu(25);
+
+echo "Suhu dalam Celsius: " . $suhu->getCelsius() . "°C<br>";
+echo "Suhu dalam Fahrenheit: " . $suhu->keFahrenheit() . "°F<br>";
+echo "Suhu dalam Kelvin: " . $suhu->keKelvin() . " K";
+?>
