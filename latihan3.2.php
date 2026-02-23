6c203a9ca11c8633f1e2b199e681b1e2483241f4
aakiphasanmutakin-stack <aakiphasanmutakin@gmail.com>
2026-02-23 11:13:31 +0700

    Add files via upload

---
+++ latihan3.2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pegadaian</title>
+</head>
+<body>
+    <h1> pinjaman <h1>
+    <form action="" method="post">
+        Besaran Pinjaman:
+        <input type="number" name="pnjm"><br>
+        Bunga %: 
+        <input type="number" name="bnga"><br>
+        Lama Angsuran:
+        <input type="number" name="bln"><br>
+        Keterlambatan Angsuran:
+        <input type="number" name="lmbt"><br>
+        <input type="submit" value="Simpan">
+    </form>
+<?php
+class angsuran{
+    $pinjam = $_POST['pnjm'];
+    $bunga = $_POST['bnga'];
+    $bulan = $_POST['bln'];
+    $lambat = $_POST['lmbt'];
+    if()
+
+}
+?>
+
+</body>
+</html>
+
+
